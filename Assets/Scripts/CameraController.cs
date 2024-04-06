9c0ab1ad41f9cda36bd70a38e9cf6ac09e259255
ShaoKuy <iqtanawad@gmail.com>
2024-04-06 11:33:29 +0700

    3.17

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,6 +12,9 @@ public class CameraController : MonoBehaviour
 
     [SerializeField] private float xInput;
     [SerializeField] private float zInput;
+    
+    [SerializeField] private Transform corner1;
+    [SerializeField] private Transform corner2;
 
     public static CameraController instance;
 
@@ -41,5 +44,16 @@ public class CameraController : MonoBehaviour
         Vector3 dir = (transform.forward * zInput) + (transform.right * xInput);
 
         transform.position += dir * moveSpeed * Time.deltaTime;
+        transform.position = Clamp(corner1.position, corner2.position);
+        
     }// Move by key
+    
+    private Vector3 Clamp(Vector3 lowerLeft, Vector3 topRight)
+    {
+        Vector3 pos = new Vector3(Mathf.Clamp(transform.position.x, lowerLeft.x, topRight.x),
+            transform.position.y,
+            Mathf.Clamp(transform.position.z, lowerLeft.z, topRight.z));
+
+        return pos;
+    } //เพิ่มเมธอด Clamp
 }
