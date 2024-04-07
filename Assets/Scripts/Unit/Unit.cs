0cb1a47354d2d1bc523eec283675298394945d38
ShaoKuy <iqtanawad@gmail.com>
2024-04-07 14:09:07 +0700

    9.9 Run Test Unit Walk

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -71,7 +71,12 @@ public class Unit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (state)
+        {
+            case UnitState.Move:
+                MoveUpdate();
+                break;
+        } //เช็ค MoveUpdate() ในลูป
     }
     
     public void ToggleSelectionVisual(bool flag)
@@ -79,5 +84,35 @@ public class Unit : MonoBehaviour
         if (selectionVisual != null)
             selectionVisual.SetActive(flag);
     } //ปิดเปิดวงแหวน
+    
+    public void SetState(UnitState toState)
+    {
+        state = toState;
+
+        if (state == UnitState.Idle)
+        {
+            navAgent.isStopped = true;
+            navAgent.ResetPath();
+        }
+    }
+    
+    public void MoveToPosition(Vector3 dest)
+    {
+        if (navAgent != null)
+        {
+            navAgent.SetDestination(dest);
+            navAgent.isStopped = false;
+        }
+
+        SetState(UnitState.Move); 
+    }
+    
+    private void MoveUpdate()
+    {
+        float distance = Vector3.Distance(transform.position, navAgent.destination);
+
+        if (distance <= 1f)
+            SetState(UnitState.Idle);
+    }
     
 }
