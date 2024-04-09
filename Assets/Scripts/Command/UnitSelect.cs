754a9f22e78b8bacb6d852407e57c2fd0a5f4d39
ShaoKuy <iqtanawad@gmail.com>
2024-04-09 11:03:24 +0700

    13.16 Test Building Select UI

--- Assets/Scripts/Command/UnitSelect.cs
+++ Assets/Scripts/Command/UnitSelect.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class UnitSelect : MonoBehaviour
 {
@@ -10,6 +11,10 @@ public class UnitSelect : MonoBehaviour
     [SerializeField]
     private Unit curUnit; //current selected single unit
     public Unit CurUnit { get { return curUnit; } }
+    
+    [SerializeField]
+    private Building curBuilding; //current selected single building
+    public Building CurBuilding { get { return curBuilding; } }
 
     private Camera cam;
     private Faction faction;
@@ -35,6 +40,9 @@ public class UnitSelect : MonoBehaviour
         //mouse down
         if (Input.GetMouseButtonDown(0))
         {
+            //if click UI, Don't clear
+            if (EventSystem.current.IsPointerOverGameObject())
+                return;
             ClearEverything();
         }
 
@@ -71,6 +79,9 @@ public class UnitSelect : MonoBehaviour
                 case "Unit":
                     SelectUnit(hit);
                     break;
+                case "Building":
+                    BuildingSelect(hit);
+                    break;
             }
         }
     } // TrySelect ในการคลิกเลือก Unit
@@ -79,12 +90,15 @@ public class UnitSelect : MonoBehaviour
     {
         if (curUnit != null)
             curUnit.ToggleSelectionVisual(false);
+        if (curBuilding != null)
+            curBuilding.ToggleSelectionVisual(false);
     } // Clear การเลือกครั้งที่แล้ว
     
     private void ClearEverything()
     {
         ClearAllSelectionVisual();
         curUnit = null;
+        curBuilding = null;
         
         //Clear UI
         InfoManager.instance.ClearAllInfo();
@@ -94,4 +108,21 @@ public class UnitSelect : MonoBehaviour
     {
         InfoManager.instance.ShowAllInfo(u);
     } // Clear และการโชว์ Stat ใน UI
+    
+    private void ShowBuilding(Building b)
+    {
+        InfoManager.instance.ShowAllInfo(b);
+    }
+    
+    private void BuildingSelect(RaycastHit hit)
+    {
+        curBuilding = hit.collider.GetComponent<Building>();
+        curBuilding.ToggleSelectionVisual(true);
+
+        if (GameManager.instance.MyFaction.IsMyBuilding(curBuilding))
+        {
+            //Debug.Log("my building");
+            ShowBuilding(curBuilding);//Show building info
+        }
+    }
 }
