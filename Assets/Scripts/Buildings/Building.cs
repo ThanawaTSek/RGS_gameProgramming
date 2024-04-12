9035e8ee6754a1f01c44406762e4554f83435d4b
ShaoKuy <iqtanawad@gmail.com>
2024-04-12 12:56:35 +0700

    15.57

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -30,6 +30,11 @@ public class Building : Structure
     [SerializeField] private float intoTheGround = 5f;
     public float IntoTheGround { get { return intoTheGround; } }
     
+    private float timer = 0f; //Constructing timer
+    public float Timer { get { return timer; } set { timer = value; } }
+    private float waitTime = 0.5f; //How fast it will be construct, higher is longer
+    public float WaitTime { get { return waitTime; } set { waitTime = value; } }
+    
     
     // Start is called before the first frame update
     void Start()
